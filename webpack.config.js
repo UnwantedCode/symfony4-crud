c9f54568f391dac2e859821d678d73cc2b541a8c
adamtarkowski <adam@dharsa.com>
2023-10-20 19:06:07 +0200

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,15 @@
 const Encore = require('@symfony/webpack-encore');
 
-// Manually configure the runtime environment if not already configured yet by the "encore" command.
-// It's useful when you use tools that rely on webpack.config.js file.
-if (!Encore.isRuntimeEnvironmentConfigured()) {
-    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
-}
-
 Encore
     .setOutputPath('public/assets/')
     .setPublicPath('/assets')
     //.addEntry('app', './assets/app.js')
-    .addEntry('css/dashboard', '[./assets/css/dashboard.css]')
-    .addEntry('css/login', '[./assets/css/login.css]')
-    .addEntry('css/dashboard', '[./assets/css/dashboard.css]')
-    // .enableStimulusBridge('./assets/controllers.json')
-    // .splitEntryChunks()
-    // .enableSingleRuntimeChunk()
+    .addStyleEntry('css/dashboard', ['./assets/css/dashboard.css'])
+    .addStyleEntry('css/login', ['./assets/css/login.css'])
+
     // .cleanupOutputBeforeBuild()
-    // .enableBuildNotifications()
     // .enableSourceMaps(!Encore.isProduction())
     // .enableVersioning(Encore.isProduction())
-    // .configureBabel((config) => {
-    //     config.plugins.push('@babel/plugin-proposal-class-properties');
-    // })
-    // .configureBabelPresetEnv((config) => {
-    //     config.useBuiltIns = 'usage';
-    //     config.corejs = 3;
-    // })
-
-;
+   ;
 
 module.exports = Encore.getWebpackConfig();
