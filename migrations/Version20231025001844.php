b156ece0c5690c7fe04471ec14e0ddd9ba15e452
adamtarkowski <adam@dharsa.com>
2023-10-25 02:33:09 +0200

    after functional tests

--- migrations/Version20231025001844.php
+++ migrations/Version20231025001844.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231024220540 extends AbstractMigration
+final class Version20231025001844 extends AbstractMigration
 {
     public function getDescription() : string
     {
